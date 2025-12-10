890b5665f7b93e09652257061020e77f6ce22eb1
claverlo <leopac84@icloud.com>
2025-12-09 19:25:03 -0800

    first commit

---
+++ scripts/variables.js
@@ -0,0 +1,70 @@
+console.log("My script is working");
+
+let firstName = "Leomar";
+let myAge = 999;
+let isProfessor = true;
+let isStudent = false;
+
+let lastName = "Claveria";
+let height = "5'7";
+
+console.log(firstName);
+console.log(myAge);
+console.log(isProfessor);
+console.log(isStudent);
+console.log(lastName);
+console.log(height);
+
+console.log("Last name: " + lastName);
+console.log("Height: " + height + " cm");
+
+
+let num1 = 10;
+let num2 = 20;
+
+let sum = num1 + num2;
+let subs = num1 - num2;
+let mul = num1 * num2;
+let div = num1 / num2;
+
+console.log("-- Arithmetic operations --");
+
+console.log("Addition of " + num1 + " and " + num2 + " is equals to " + sum);
+
+console.log(`Subtraction of ${num1} and ${num2} is equals to ${subs}`);
+console.log(`Multiplication of ${num1} and ${num2} is equals to ${mul}`);
+console.log(`Division of ${num1} and ${num2} is equals to ${div}`);
+
+let pi = 3.1416;
+let radius = 3;
+let circleArea = pi * radius * radius;
+
+console.log(`The area of a circle with ${radius} is ${circleArea}`);
+
+// difference between variable and constant
+// create the variable
+let aVariable;
+
+// assign the value
+aVariable = 30;
+
+// create and assign the value
+let anotherVariable = 50;
+
+console.log(aVariable, anotherVariable);
+
+
+const weekDays = 7;
+// weekDays = 9;
+console.log(weekDays);
+
+// constant won't change
+const daysInWeek = 7;
+
+// the variables change
+let vacationDays = 14;
+
+let totalDays = daysInWeek + vacationDays;
+
+// Now vacation change
+vacationDays = 21;
